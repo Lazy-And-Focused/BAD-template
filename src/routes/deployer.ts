ac3e51a7c95b96245921aa828c7b97206cd4245e
fockusty <wwddwww@mail.ru>
2025-08-11 19:28:33 +0500

    -up agreements/general > -add import and export, -up links

---
+++ src/routes/deployer.ts
@@ -0,0 +1,28 @@
+import { join, parse } from "path";
+import { readdirSync } from "fs";
+import { NestModule } from "@nestjs/common";
+
+const EXTENSION = parse(__filename).ext;
+const FILE_FORMAT = new RegExp("[\\w\\W]+\\.module\\" + EXTENSION, "g");
+
+export class Deployer {
+  public execute(filesPath = __dirname, data = <NestModule[]>[]) {
+    const files = readdirSync(filesPath);
+
+    for (const folder of files) {
+      try {
+        this.execute(join(filesPath, folder), data);
+      } catch {
+        if (!FILE_FORMAT.test(folder)) continue;
+
+        const module = require(join(filesPath, folder)).default;
+
+        data.push(module);
+      }
+    }
+
+    return data;
+  }
+}
+
+export default Deployer;
---
+++ src/services/auth.service.ts
@@ -0,0 +1,71 @@
+import { Next, Req, Res } from "@nestjs/common";
+import { Auth, AUTH_TYPES, AuthTypes } from "@types";
+import type { NextFunction, Request, Response } from "express";
+
+import passport = require("passport");
+
+const abbreviations: Map<string, AuthTypes> = new Map([]);
+
+class AuthApi {
+  private readonly _method: string;
+
+  public constructor(method: string) {
+    this._method = method;
+  }
+
+  static get methods(): Record<"abbreviations" | "methods", readonly string[]> {
+    return {
+      abbreviations: Array.from(abbreviations.keys()),
+      methods: AUTH_TYPES
+    };
+  }
+
+  private getMethod(): [boolean, { [key: string]: unknown; method: string; body: unknown }] {
+    if ((AUTH_TYPES as unknown as string[]).includes(this._method)) {
+      return [true, { body: null, method: this._method }];
+    }
+
+    const abbreviation = abbreviations.get(this._method); 
+    if (abbreviation) {
+      return [true, { body: null, method: abbreviation }];
+    };
+
+    return [
+      false,
+      {
+        body: {
+          msg: "Sorry, but method " + this._method + " not found. Try next:",
+          methods: AUTH_TYPES
+        },
+        method: this._method
+      }
+    ];
+  }
+
+  public auth(@Req() req: Request, @Res() res: Response, @Next() next: NextFunction): unknown {
+    const [successed, { method, body }] = this.getMethod();
+
+    if (!successed) {
+      return res.send(body)
+    };
+
+    return passport.authenticate(method)(req, res, next);
+  }
+
+  public callback(
+    @Req() req: Request,
+    @Res() res: Response,
+    @Next() next: NextFunction,
+    callback: (...args: [unknown, Auth | null, unknown]) => unknown
+  ): unknown {
+    const [successed, { method, body }] = this.getMethod();
+
+    if (!successed) {
+      return res.send(body)
+    };
+
+    return passport.authenticate(method, callback)(req, res, next);
+  }
+}
+
+export default AuthApi;
