766be9f18abc822fcb828de48e75a4c0a3219826
fockusty <wwddwww@mail.ru>
2025-08-14 09:46:56 +0500

    -up: types, package.json

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,26 @@
 import { Module, NestModule, MiddlewareConsumer } from "@nestjs/common";
 import { LoggerMiddleware } from "./middleware/logger.middleware";
-import { RouterModule } from "@nestjs/core";
+import { APP_INTERCEPTOR, RouterModule } from "@nestjs/core";
+import { CacheModule, CacheInterceptor } from '@nestjs/cache-manager';
 
 import AuthModule from "./routes/auth/auth.module";
 
 @Module({
   imports: [
-    AuthModule,
-  ].flatMap((module) => [module, RouterModule.register([{ path: "api", module }])])
+    ...[
+      AuthModule,
+    ].flatMap((module) => [module, RouterModule.register([{ path: "api", module }])]),
+    CacheModule.register({
+      ttl: 5 * 60 * 1000, // 5 minutes
+      isGlobal: true
+    })
+  ],
+  providers: [
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: CacheInterceptor
+    }
+  ]
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
---
+++ src/types/promise/auth.types.ts
@@ -0,0 +1,9 @@
+import type {
+  Auth as IAuth,
+  AuthTypes as IAuthTypes
+} from "../auth.types";
+
+export { AUTH_TYPES } from "../auth.types";
+
+export type Auth = Promise<IAuth>;
+export type AuthTypes = Promise<IAuthTypes>;
