9240579d2a7242042cd9565df5bc301d9a462113
fockusty <wwddwww@mail.ru>
2025-08-17 11:56:27 +0500

    -save

--- bad-fockarch/src/bin/index.ts
+++ bad-fockarch/src/bin/index.ts
@@ -1,18 +1,50 @@
 #!/usr/bin/env node
 import yargs from "yargs";
-import { hideBin } from 'yargs/helpers';
+import { hideBin } from "yargs/helpers";
+
+// import { exec } from "child_process";
+// const RELEASE_URL = "https://api.github.com/repos/Lazy-And-Focused/BAD-template/releases/latest";
 
 yargs()
   .scriptName("bad")
-  .usage('$0 <cmd> [args]')
-  .command('hello [name] [data]', 'welcome ter yargs!', (yargs) => {
-    yargs.positional('name', {
-      type: 'string',
-      default: 'Cambi',
-      describe: 'the name to say hello to'
+  .usage("$0 <cmd> [args]")
+  .command("create [name] [path] [package-manager]", "create your backend app with BAD", (yargs) => {
+    yargs.positional("name", {
+      type: "string",
+      default: "bad-app",
+      describe: "name of your app"
     })
-  }, function (argv) {
-    console.log(argv);
+
+    yargs.positional("path", {
+      type: "string",
+      default: "/",
+      describe: "path to your root"
+    });
+
+    yargs.positional("package-manager", {
+      type: "string",
+      default: "npm",
+      describe: "Package manager (npm/pnpm)",
+      alias: "pm"
+    });
+  }, async function (argv) {
+    console.log("Название: " + argv.name);
+    console.log("Путь: " + argv.path);
+    console.log("Пакетный менеджер: " + argv.packageManager);
+
+    // exec('npm i', function (error, stdout, stderr) {
+    //   console.log('stdout: ' + stdout);
+    //   console.log('stderr: ' + stderr);
+    //   if (error !== null) {
+    //     console.log('exec error: ' + error);
+    //   }
+    // });
+
+    // const data = (await fetch(RELEASE_URL, {
+      // method: "GET"
+    // })).json();
+
+    // console.log(data);
   })
   .help()
   .parse(hideBin(process.argv));
