a5d9165df65bfb57ba8a84ee449d407c7161a65a
FOCKUSTY <wwddwwwd@mail.ru>
2025-09-20 22:24:30 +0500

    -add http errors

--- src/routes/error/error.controller.ts
+++ src/routes/error/error.controller.ts
@@ -1,13 +1,24 @@
-import { Controller as NestController, Get, Injectable } from "@nestjs/common";
+import { Controller as NestController, Get, Injectable, HttpException, Query, HttpStatus } from "@nestjs/common";
 
-import { ROUTE } from "./error.routes";
+import { ROUTE, ROUTES } from "./error.routes";
 
 @Injectable()
 @NestController(ROUTE)
 export class Controller {
-  @Get()
+  @Get(ROUTES.GET)
   public get() {
     throw new Error("Test error for sentry");
+  }
+
+  @Get(ROUTES.GET_HTTP)
+  public getHttp(
+    @Query("status") status?: string
+  ) {
+    if (!status) {
+      throw new HttpException("Not found TEST", HttpStatus.NOT_FOUND);
+    }
+
+    throw new HttpException("Exeption TEST", +status);
   }
 }
 
--- src/routes/error/error.routes.ts
+++ src/routes/error/error.routes.ts
@@ -1,1 +1,6 @@
 export const ROUTE = "error";
+
+export const ROUTES = {
+  GET: "/",
+  GET_HTTP: "/http"
+};
